8108061b6cecf899d082a730f4c8908fd4c3ea43
Mohamedseffine <seffinemohamed470@gmail.com>
2025-11-09 10:12:26 +0100

    colors in rectangle

--- geometrical_shapes/Rectangle.java
+++ geometrical_shapes/Rectangle.java
@@ -12,37 +12,18 @@ public class Rectangle implements Drawable {
     }
 
     public void draw(Displayable displayable) {
-        Color color = this.getColor();
         int xMin = Math.min(this.point1.getX(), this.point2.getX());
         int xMax = Math.max(this.point1.getX(), this.point2.getX());
         int yMin = Math.min(this.point1.getY(), this.point2.getY());
         int yMax = Math.max(this.point1.getY(), this.point2.getX());
     
-        Line l1 = new Line(new Point(xMin, yMin), new Point(xMin, yMax));
-        Line l2 = new Line(new Point(xMin, yMin), new Point(xMax, yMin));
-        Line l3 = new Line(new Point(xMax, yMax), new Point(xMin, yMax));
-        Line l4 = new Line(new Point(xMax, yMax), new Point(xMax, yMin));
-        drawl(displayable, l1);
-        drawl(displayable, l2);
-        drawl(displayable, l3);
-        drawl(displayable, l4);
-    }
-    public void drawl(Displayable displayable, Line l) {
-        float dx = l.p2.getX() - l.p1.getX();
-        float dy = l.p2.getY() - l.p1.getY();
-
-        float steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
-
-        float Xinc = dx / steps;
-        float Yinc = dy / steps;
-
-        float x = l.p1.getX();
-        float y = l.p1.getY();
-
-        for (int i = 0; i < steps; i++) {
-            displayable.display(Math.round(x), Math.round(y), this.color);
-            x += Xinc;
-            y += Yinc;
-        }
+        Line l1 = new Line(new Point(xMin, yMin), new Point(xMin, yMax), this.color);
+        Line l2 = new Line(new Point(xMin, yMin), new Point(xMax, yMin), this.color);
+        Line l3 = new Line(new Point(xMax, yMax), new Point(xMin, yMax), this.color);
+        Line l4 = new Line(new Point(xMax, yMax), new Point(xMax, yMin), this.color);
+        l1.draw(displayable);
+        l2.draw(displayable);
+        l3.draw(displayable);
+        l4.draw(displayable);
     }
 }
